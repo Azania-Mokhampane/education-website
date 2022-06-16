a598ac227c4bd502b005b2b8c12635cb2e860a32
Azania Dev <azaniam04@gmail.com>
2022-06-16 18:15:22 +0200

    ref: fixed the color mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        78: "300px",
+        105: "500px",
+      },
+    },
   },
   variants: {
     extend: {},
